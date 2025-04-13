87d894604315ed17b93dee1b5c9ea59cc7fa9989
hsandoval <henk.sandoval@hotmail.com>
2025-04-13 17:42:11 +0200

    test: ✅ Integrate AOS library for animations and implement AfterViewInit lifecycle hook

--- src/app/features/MyResume/home/home.component.ts
+++ src/app/features/MyResume/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, inject, signal, ViewEncapsulation } from '@angular/core';
 import { HeaderComponent } from '../layouts/header/header.component';
 import { CoverComponent } from './components/cover/cover.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -7,15 +7,16 @@ import { ActivatedRoute } from '@angular/router';
 import { StateService } from '@core/services/state/state.service';
 import { FooterComponent } from '../layouts/footer/footer.component';
 import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
+import AOS from 'aos';
 
 @Component({
-    selector: 'app-home',
-    imports: [HeaderComponent, CoverComponent, FooterComponent, ProfileComponent, AppIdDirective],
-    templateUrl: './home.component.html',
-    styles: ``,
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-home',
+  imports: [HeaderComponent, CoverComponent, FooterComponent, ProfileComponent, AppIdDirective],
+  templateUrl: './home.component.html',
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   activatedRoute = inject(ActivatedRoute);
   stateService = inject(StateService);
   sections = signal(MENU_SECTIONS);
@@ -26,15 +27,22 @@ export class HomeComponent {
       this.stateService.userName.set(userName);
     });
 
-    this.loadStyle('https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css');
     this.loadStyle('https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css');
     this.loadStyle(
       'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css'
     );
     this.loadStyle('https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.4/css/boxicons.min.css');
-    this.loadScript('https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js');
     this.loadScript('https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js');
     this.loadScript('js/main.js');
+  }
+
+  ngAfterViewInit(): void {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false,
+    });
   }
 
   private loadStyle(url: string): void {
