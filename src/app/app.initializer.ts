066d86750fc9b204c2025d5eb606e4c4cb954729
hsandoval <henk.sandoval@hotmail.com>
2025-04-13 14:51:24 +0200

    feat: ⚡️ Refactor app initialization to use async/await for loading configuration

--- src/app/app.initializer.ts
+++ src/app/app.initializer.ts
@@ -1,26 +1,29 @@
 import { inject } from '@angular/core';
-import { catchError, tap, throwError } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { ConfigService } from '@core/services/config/config.service';
 import { parseDTO } from '@core/services/config/interfaces/config';
 import { HttpService } from '@core/services/http/http.service';
 
-export const initializeApp = () => {
+export const initializeApp = async () => {
   const httpService = inject(HttpService);
   const configService = inject(ConfigService);
   const url = './config/config.json';
 
-  httpService.get(url).pipe(
-    tap((configData) => {
-      const dto = parseDTO(configData);
-      if (dto.success) {
-        configService.setConfig(dto.data);
-      } else {
-        console.error('Invalid config.json', dto.error);
-      }
-    }),
-    catchError((error) => {
-      console.error('Error loading config.json', error);
-      return throwError(() => new Error('Error loading config.json', error));
-    })
-  ).subscribe();
+  if (configService.isConfigLoaded()) {
+    return;
+  }
+
+  try {
+    const configData = await firstValueFrom(httpService.get(url));
+    const dto = parseDTO(configData);
+
+    if (dto.success) {
+      configService.setConfig(dto.data);
+    } else {
+      console.error('Invalid config.json', dto.error);
+    }
+  } catch (error) {
+    console.error('Error loading config.json', error);
+    throw new Error('Error loading config.json');
+  }
 };
