fe1ed41b99bf77410b260e47979b6a13e328156d
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-04-24 21:32:23 +0200

    feat: ✨ Update summary section to reflect new introduction and remove personal detail

--- src/app/features/MyResume/home/components/profile/components/summary/interfaces/summary.ts
+++ src/app/features/MyResume/home/components/profile/components/summary/interfaces/summary.ts
@@ -1,12 +1,8 @@
-import { Contact } from '../../contact/interfaces/contact';
 import { Education } from './education';
 import { WorkExperience } from './work-experience';
 
 export interface Summary {
-  about: string;
-  name: string;
   introduction: string;
-  contactProfile: Contact;
   education: Education;
   workExperience: WorkExperience;
 }
--- src/app/features/MyResume/home/components/profile/components/summary/mocks/summary.mock.ts
+++ src/app/features/MyResume/home/components/profile/components/summary/mocks/summary.mock.ts
@@ -1,14 +1,9 @@
-import { mockContact } from '../../contact/mocks/contact-profile.mock';
 import { Summary } from '../interfaces/summary';
 import { mockEducation } from './education.mock';
 import { mockWorkExperience } from './work-experience.mock';
 
 export const mockSummary: Summary = {
-  about: 'I am a software developer',
-  name: 'Brahando test',
-  introduction:
-    'Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.',
-  contactProfile: mockContact,
+  introduction: 'Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.',
   workExperience: mockWorkExperience,
   education: mockEducation,
 };
