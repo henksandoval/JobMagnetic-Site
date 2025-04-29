d2ed2532fd8cc5dffb2215dcbe4e0701151ddfde
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-04-29 13:56:30 +0200

    feat: ✨ Update tests in AdminComponent to reflect changes in Early Adopter view

--- src/app/features/MyResume/admin/admin.component.spec.ts
+++ src/app/features/MyResume/admin/admin.component.spec.ts
@@ -15,12 +15,20 @@ describe(AdminComponent.name, () => {
     expect(screen.getByTestId('header_title')).toHaveTextContent('Vista Early Adopter');
   });
 
-  it('should show welcome', () => {
-    expect(screen.getByTestId('welcome')).toHaveTextContent('Bienvenido a Early Adopter');
+  it('should show what is a view', () => {
+    expect(screen.getByTestId('title_what_is')).toHaveTextContent('¿Qué es una Vista Early Adopter?');
   });
 
-  it('should show what is a view', () => {
-    expect(screen.getByTestId('view')).toHaveTextContent('¿Qué es una Vista Early Adopter?');
+  it('It should show the key features', () => {
+    expect(screen.getByTestId('title_features')).toHaveTextContent('Características Clave');
+  });
+
+  it('It should show how you can help us.', () => {
+    expect(screen.getByTestId('title_help')).toHaveTextContent('¿Cómo puedes ayudarnos?');
+  });
+
+  it('It should show the message I want to be an early adopter!', () => {
+    expect(screen.getByTestId('button_join')).toHaveTextContent('¡Quiero ser Early Adopter!');
   });
 
 });
