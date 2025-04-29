02d94dd4bfaedaba03ceed19392955f9d780fe68
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-04-29 12:18:35 +0200

    feat: ✨ Implement OnPush change detection strategy in AdminComponent

--- src/app/features/MyResume/admin/admin.component.ts
+++ src/app/features/MyResume/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
 
 
@@ -7,6 +7,8 @@ import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
   imports: [AppIdDirective],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdminComponent{
+
 }
