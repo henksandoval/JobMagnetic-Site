d65d705e0ffdc534cdfa3c3cdbe0b56b4d2bfe9c
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-01 17:05:47 +0200

    feat: ✨ Refactor registration component to use reactive forms and improve data handling

--- src/app/features/MyResume/admin/account/register/register.component.ts
+++ src/app/features/MyResume/admin/account/register/register.component.ts
@@ -1,62 +1,61 @@
-import { Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProfileService } from '../../../home/services/profile.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   currentStep = 0;
   steps = [0, 1, 2];
+  personalDataForm!: FormGroup;
+  isSaving = false;
+  urlEndpoints: string | undefined;
 
+  constructor(
+    private fb: FormBuilder,
+    private profileService: ProfileService
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.urlEndpoints = this.profileService.getEndpoints().profile.personalData;
+  }
   // Datos del formulario
-  formData = {
-    fullName: '',
-    email: '',
-    phone: '',
-    description: '',
-    birthday: '',
-    city: '',
-    testimonialName: '',
-    jobTitle: '',
-    testimonial: ''
-  };
+  private initializeForm(): void {
+    this.personalDataForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      profileImageUrl: [''],
+      birthDate: [''],
+      middleName: [''],
+      secondLastName: [''],
+    });
+  }
 
-  nextStep() {
-    if (this.currentStep < this.steps.length - 1) {
-      this.currentStep++;
-    } else {
-      this.completeWizard();
+  savePersonalData(): void {
+    if (this.isSaving) {
+      return;
     }
-  }
 
-  prevStep() {
-    if (this.currentStep > 0) {
-      this.currentStep--;
+    this.isSaving = true;
+    const urlEndpoint = this.urlEndpoints;
+    const personalData = this.personalDataForm.value;
+    if (urlEndpoint != null) {
+      this.profileService.saveData(urlEndpoint, personalData).subscribe(
+        (response) => {
+          console.log(response);
+          alert('¡Datos guardados exitosamente!');
+          this.isSaving = false;
+        },
+        (error) => {
+          console.error('Error al guardar los datos:', error);
+        }
+      );
     }
-  }
-
-  completeWizard() {
-    console.log('Datos del formulario:', this.formData);
-    alert('¡Registro completado!');
-    this.resetForm();
-  }
-
-  resetForm() {
-    this.formData = {
-      fullName: '',
-      email: '',
-      phone: '',
-      description: '',
-      birthday: '',
-      city: '',
-      testimonialName: '',
-      jobTitle: '',
-      testimonial: ''
-    };
-    this.currentStep = 0;
   }
 }
