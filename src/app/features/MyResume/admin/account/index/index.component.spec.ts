76bd18869b48fc28d54fd6ce0a48967121f9b1b5
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-02 14:24:12 +0200

    feat: ✨ Update localization keys and improve accessibility in index and register components

--- src/app/features/MyResume/admin/account/index/index.component.spec.ts
+++ src/app/features/MyResume/admin/account/index/index.component.spec.ts
@@ -12,23 +12,23 @@ describe(IndexComponent.name, () => {
   });
 
   it('should display title', () => {
-    expect(screen.getByTestId('header_title')).toHaveTextContent('Vista Early Adopter');
+    expect(screen.getByTestId('vista Early adopter')).toHaveTextContent('Vista Early Adopter');
   });
 
   it('should show what is a view', () => {
-    expect(screen.getByTestId('title_what_is')).toHaveTextContent('¿Qué es una Vista Early Adopter?');
+    expect(screen.getByTestId('qué es una vista early adopter')).toHaveTextContent('¿Qué es una Vista Early Adopter?');
   });
 
   it('It should show the key features', () => {
-    expect(screen.getByTestId('title_features')).toHaveTextContent('Características Clave');
+    expect(screen.getByTestId('características clave')).toHaveTextContent('Características Clave');
   });
 
   it('It should show how you can help us.', () => {
-    expect(screen.getByTestId('title_help')).toHaveTextContent('¿Cómo puedes ayudarnos?');
+    expect(screen.getByTestId('cómo puedes ayudarnos')).toHaveTextContent('¿Cómo puedes ayudarnos?');
   });
 
   it('It should show the message I want to be an early adopter!', () => {
-    expect(screen.getByTestId('button_join')).toHaveTextContent('¡Quiero ser Early Adopter!');
+    expect(screen.getByTestId('quiero ser early adopter')).toHaveTextContent('¡Quiero ser Early Adopter!');
   });
 
 });
