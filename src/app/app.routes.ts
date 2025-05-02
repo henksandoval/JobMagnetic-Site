c51c440ff4b6018d2c5cd1b38007ae591238568b
hsandoval <henk.sandoval@hotmail.com>
2025-05-03 00:08:09 +0200

    refactor: 🚚 Rename admin components and update paths for early adopter structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { homeRoutes } from './features/cv-viewer/home/home.routes';
-import { adminRoutes } from './features/cv-manager/admin/admin.routes';
+import { adminRoutes } from './features/cv-manager/early-adopter/admin.routes';
 import { PageNotFoundComponent } from './features/cv-viewer/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   ...adminRoutes,
   {
-    path: 'admin', loadChildren: () => import('./features/cv-manager/admin/admin.routes').then(m => m.adminRoutes),
+    path: 'early-adopter', loadChildren: () => import('./features/cv-manager/early-adopter/admin.routes').then(m => m.adminRoutes),
   },
   ...homeRoutes,
   {
