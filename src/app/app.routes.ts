3e0932324fb65f66166d84890de1468957d8a116
hsandoval <henk.sandoval@hotmail.com>
2025-05-04 14:00:09 +0200

    refactor: ⚡️ Use lazy loading by improve rendering performance in cv-manager

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 import { myResumeRoutes } from './features/cv-viewer/my-resume/myResumeRoutes';
-import { cvManagerRoutes } from './features/cv-manager/cv-manager.routes';
 import { PageNotFoundComponent } from './features/cv-viewer/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  ...cvManagerRoutes,
   {
-    path: 'early-adopter', loadChildren: () => import('./features/cv-manager/cv-manager.routes').then(m => m.cvManagerRoutes),
+    path: 'cv-manager',
+    loadChildren: () =>
+      import('./features/cv-manager/cv-manager.routes').then((m) => m.cvManagerRoutes),
   },
   ...myResumeRoutes,
   {
     path: '',
-    redirectTo: '/my-resume/john',
+    redirectTo: 'my-resume/john',
     pathMatch: 'full',
   },
   {
--- src/app/features/cv-manager/cv-manager.routes.ts
+++ src/app/features/cv-manager/cv-manager.routes.ts
@@ -1,23 +1,15 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { EarlyAdopterComponent } from './early-adopter/early-adopter.component';
-import { NgModule } from '@angular/core';
 import { IndexComponent } from './early-adopter/components/index/index.component';
 import { RegisterComponent } from './early-adopter/components/register/register.component';
 
 export const cvManagerRoutes: Routes = [
   {
-    path: '',
+    path: 'early-adopter',
     component: EarlyAdopterComponent,
     children: [
-      { path: '', redirectTo: 'index', pathMatch: 'full' },
-      { path: 'index', component: IndexComponent },
+      { path: '', component: IndexComponent },
       { path: 'register', component: RegisterComponent },
     ],
   },
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(cvManagerRoutes), IndexComponent],
-  exports: [RouterModule],
-})
-export class AdminRoutes {}
