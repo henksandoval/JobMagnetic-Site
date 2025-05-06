bc465aad7a5d90f6621c59b7f5e6c9e754395f0c
hsandoval <henk.sandoval@hotmail.com>
2025-05-06 19:19:45 +0200

    feat: ✨ Add ResumeFormComponent with initial template and unit tests

---
+++ src/app/features/cv-manager/early-adopter/components/register/resume-form/resume-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-form',
+  imports: [],
+  templateUrl: './resume-form.component.html',
+  styles: ``
+})
+export class ResumeFormComponent {
+
+}
