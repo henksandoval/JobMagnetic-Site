200a62de9c16eeac87fe7dbfe4f4be902f3995fe
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-07 17:32:39 +0200

    feat: ✨ Add saveResumeData method to ResumeFormComponent for saving profile data

--- src/app/features/cv-manager/early-adopter/components/register/resume-form/resume-form.component.ts
+++ src/app/features/cv-manager/early-adopter/components/register/resume-form/resume-form.component.ts
@@ -1,19 +1,37 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
+import { NgIf } from '@angular/common';
+import { ProfileService } from '../../../services/profile.service';
+import { ApiEndpoints } from '@core/constants/api-endpoints';
 
 @Component({
   selector: 'app-resume-form',
-  imports: [ReactiveFormsModule, AppIdDirective],
+  imports: [ReactiveFormsModule, AppIdDirective, NgIf],
   templateUrl: './resume-form.component.html',
   styles: ``,
 })
 export class ResumeFormComponent implements OnInit {
+  private readonly profileService: ProfileService = inject(ProfileService);
   private readonly formBuilder: FormBuilder = inject(FormBuilder);
   dataForm!: FormGroup;
 
   ngOnInit(): void {
     this.initializeForm();
+  }
+
+  saveResumeData(): void {
+    const urlEndpoint = ApiEndpoints.profile.about;
+    const resumeData = this.dataForm.value;
+
+    this.profileService.saveData(urlEndpoint, resumeData).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error('Error saving resume data:', error);
+      }
+    );
   }
 
   private initializeForm(): void {
