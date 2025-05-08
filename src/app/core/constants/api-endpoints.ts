aaf622bf45c77dc3a866f4fbfd620617870c27a1
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-08 17:19:35 +0200

    feat: ✨ Update about endpoint in ApiEndpoints for resume API integration

--- src/app/core/constants/api-endpoints.ts
+++ src/app/core/constants/api-endpoints.ts
@@ -1,7 +1,7 @@
 export const ApiEndpoints = {
   profile: {
     personalData: 'v1/profile',
-    about: 'v1/profile/about',
+    about: '/api/v1/resume',
     service: 'v1/profile/service',
     skillSet: 'v1/profile/skillSet',
     portfolio: 'v1/profile/portfolio',
