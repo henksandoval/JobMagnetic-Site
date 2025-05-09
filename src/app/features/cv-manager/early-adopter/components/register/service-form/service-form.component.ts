c2dfd7b337f928cb8a32d06e2e66929c7c793f59
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-09 12:46:35 +0200

    feat: ✨ Create ServiceFormComponent for service management

---
+++ src/app/features/cv-manager/early-adopter/components/register/service-form/service-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-service-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: 'service-form.component.html',
+  styles: ``,
+})
+export class ServiceFormComponent {}
