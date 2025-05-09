7140563af2c5294c2fc37464ec2a3d94a28b2463
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-09 12:45:37 +0200

    feat: ✨ Add ServiceFormComponent to RegisterComponent for enhanced functionality

--- src/app/features/cv-manager/early-adopter/components/register/register.component.ts
+++ src/app/features/cv-manager/early-adopter/components/register/register.component.ts
@@ -2,13 +2,15 @@ import { Component, signal, WritableSignal } from '@angular/core';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
 import { ResumeFormComponent } from './resume-form/resume-form.component';
+import { ServiceFormComponent } from './service-form/service-form.component';
 
 @Component({
   selector: 'app-register',
-  imports: [AppIdDirective, ProfileFormComponent, ResumeFormComponent],
+  imports: [AppIdDirective, ProfileFormComponent, ResumeFormComponent, ServiceFormComponent],
   template: `
     <app-profile-form appId="profile-component"></app-profile-form>
     <app-resume-form appId="resume-component"></app-resume-form>
+    <app-service-form appId="service-component"></app-service-form>
   `,
 })
 export class RegisterComponent {
