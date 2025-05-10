7ed8216a99b60928704096eb294321dc3673eb99
hsandoval <henk.sandoval@hotmail.com>
2025-05-10 17:40:39 +0200

    chore: 🚧 Add summary form with education and work experience sections, including modals for data entry

--- src/app/features/cv-manager/early-adopter/components/register/summary-form/summary-form.component.ts
+++ src/app/features/cv-manager/early-adopter/components/register/summary-form/summary-form.component.ts
@@ -1,24 +1,94 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-summary-form',
-  imports: [ReactiveFormsModule, AppIdDirective],
+  imports: [ReactiveFormsModule, AppIdDirective, DatePipe],
   templateUrl: './summary-form.component.html',
   styles: ``,
 })
 export class SummaryFormComponent implements OnInit {
-  private readonly formBuilder: FormBuilder = inject(FormBuilder);
   dataForm!: FormGroup;
+  educationForm!: FormGroup;
+  workExperienceForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.initializeForm();
+    this.dataForm = this.formBuilder.group({
+      introduction: ['', [Validators.required]],
+      education: this.formBuilder.array([]),
+      workExperiences: this.formBuilder.array([]),
+    });
+
+    this.educationForm = this.formBuilder.group({
+      degree: ['', [Validators.required]],
+      institutionName: ['', [Validators.required]],
+      institutionLocation: ['', [Validators.required]],
+      description: [''],
+      startDate: ['', [Validators.required]],
+      endDate: [''],
+    });
+
+    this.workExperienceForm = this.formBuilder.group({
+      jobTitle: ['', [Validators.required]],
+      companyName: ['', [Validators.required]],
+      companyLocation: ['', [Validators.required]],
+      description: [''],
+      startDate: ['', [Validators.required]],
+      endDate: [''],
+    });
   }
 
-  private initializeForm(): void {
-    this.dataForm = this.formBuilder.group({
-      introduction: [''],
-    });
+  get educationFormArray(): FormArray {
+    return this.dataForm.get('education') as FormArray;
+  }
+
+  get workExperienceFormArray(): FormArray {
+    return this.dataForm.get('workExperiences') as FormArray;
+  }
+
+  confirmAddEducation(): void {
+    if (this.educationForm.valid) {
+      this.educationFormArray.push(this.formBuilder.group(this.educationForm.value));
+      console.log('Education added:', this.educationForm.value);
+
+      this.educationForm.reset();
+    } else {
+      console.log('Invalid education form');
+    }
+  }
+
+  confirmAddWorkExperience(): void {
+    if (this.workExperienceForm.valid) {
+      this.workExperienceFormArray.push(this.formBuilder.group(this.workExperienceForm.value));
+      console.log('Work Experience added:', this.workExperienceForm.value);
+
+      this.workExperienceForm.reset();
+    } else {
+      console.log('Invalid work experience form');
+    }
+  }
+
+  removeEducation(index: number): void {
+    this.educationFormArray.removeAt(index);
+    console.log('Education removed at index:', index);
+  }
+
+  removeWorkExperience(index: number): void {
+    this.workExperienceFormArray.removeAt(index);
+    console.log('Work experience removed at index:', index);
+  }
+
+  onSubmit(): void {
+    if (this.dataForm.valid) {
+      const formData = this.dataForm.value;
+      console.log('Form submitted:', formData);
+
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
