6ab64e1cd9da146c41893ae7f4c1c5ab425c8edc
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-14 12:05:12 +0200

    feat: ✨ Update service endpoint URL in profile to include API prefix

--- src/app/core/constants/api-endpoints.ts
+++ src/app/core/constants/api-endpoints.ts
@@ -2,7 +2,7 @@ export const ApiEndpoints = {
   profile: {
     personalData: 'v1/profile',
     about: '/api/v1/resume',
-    service: 'v1/profile/service',
+    service: '/api/v1/service',
     skillSet: 'v1/profile/skillSet',
     portfolio: 'v1/profile/portfolio',
   },
