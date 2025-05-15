8bb6d76fe2045b1a2627030d38d2fc46c15dae69
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-15 13:30:43 +0200

    feat: ✨ Add @testing-library/user-event for enhanced user interaction testing in service form component

--- src/app/features/cv-manager/early-adopter/components/register/service-form/service-form.component.spec.ts
+++ src/app/features/cv-manager/early-adopter/components/register/service-form/service-form.component.spec.ts
@@ -4,6 +4,9 @@ import '@testing-library/jest-dom';
 import '@angular/localize/init';
 import translations from '../../../../../../../assets/i18n/messages.json';
 import { ServiceBase } from '../models/serviceBase.model';
+import userEvent from '@testing-library/user-event';
+import { ComponentFixture } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const overviewEntrie: ServiceBase = {
   overview: 'overview',
@@ -21,66 +24,33 @@ const overviewEntrie: ServiceBase = {
   ],
 };
 
-
-
 describe(ServiceFormComponent.name, () => {
-  let component: ServiceFormComponent;
+  const user = userEvent.setup();
+  let componentFixture: ComponentFixture<ServiceFormComponent>;
 
   beforeEach(async () => {
     const { fixture } = await render(ServiceFormComponent, {
+      imports: [ReactiveFormsModule],
     });
-    component = fixture.componentInstance;
+    componentFixture = fixture;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('It should show the title of', async () => {
+    expect(screen.getByTestId('serviceData')).toHaveTextContent('Step 6: Service Data');
   });
 
-  it('should initialize the form correctly', () => {
-    component.ngOnInit();
-    expect(component.serviceDataForm).toBeDefined();
-    expect(component.serviceDataForm.controls['overview']).toBeDefined();
-    expect(component.serviceDataForm.controls['position']).toBeDefined();
-    expect(component.serviceDataForm.controls['title']).toBeDefined();
-    expect(component.serviceDataForm.controls['description']).toBeDefined();
-    expect(component.serviceDataForm.controls['urlLink']).toBeDefined();
-    expect(component.serviceDataForm.controls['urlImage']).toBeDefined();
-    expect(component.serviceDataForm.controls['urlVideo']).toBeDefined();
-    expect(component.serviceDataForm.controls['type']).toBeDefined();
-  });
+  describe('I should add data in Overview', () => {
+    it('Must display in data view', () => {
+      const overview = overviewEntrie.overview;
+      await setEducationEntrie(overview);
 
-  describe('Should display correct translations in the screen: ', () => {
-    const testCases = [
-      { testId: 'serviceData', key: 'serviceData' },
-      { testId: 'overview', key: 'overview' },
-      { testId: 'position', key: 'position' },
-      { testId: 'title', key: 'title' },
-      { testId: 'description', key: 'description' },
-      { testId: 'urlLink', key: 'urlLink' },
-      { testId: 'urlImage', key: 'urlImage' },
-      { testId: 'urlVideo', key: 'urlVideo' },
-      { testId: 'type', key: 'type' },
-    ];
-
-    testCases.forEach(({ testId, key }) => {
-      it(`${key}`, () => {
-        if (!(key in translations.translations)) {
-          throw new Error(`Missing translation for key: ${key}`);
-        }
-
-        const element = screen.getByTestId(testId);
-
-        const expectedTranslation = translations.translations[key as keyof typeof translations.translations];
-        expect(element).toBeInTheDocument();
-        expect(element).toHaveTextContent(expectedTranslation);
-      });
     });
+
+    const setEducationEntrie = async (serviceBase: ServiceBase) => {
+      const overviewInput = screen.getByTestId('inputOverview');
+
+
+    };
   });
 
-  it('should call saveServiceData when the save button is clicked', async () => {
-    const saveServiceData = jest.spyOn(component, 'saveServiceData').mockImplementation(() => {});
-    const saveButton = screen.getByRole('button', { name: /save/i });
-    saveButton.click();
-    expect(saveServiceData).toHaveBeenCalled();
-  });
 });
