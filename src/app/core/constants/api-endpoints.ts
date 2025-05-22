16a93461df23bf85a60e513ee6ba5f5a982968a2
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-22 21:50:49 +0200

    feat: ✨ Add summary endpoint to profile API

--- src/app/core/constants/api-endpoints.ts
+++ src/app/core/constants/api-endpoints.ts
@@ -2,6 +2,7 @@ export const ApiEndpoints = {
   profile: {
     personalData: 'v1/profile',
     about: '/api/v1/resume',
+    summary: '/api/v1/summary',
     service: '/api/v1/service',
     skillSet: 'v1/profile/skillSet',
     portfolio: 'v1/profile/portfolio',
