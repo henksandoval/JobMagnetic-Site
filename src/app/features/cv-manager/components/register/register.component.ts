57559c2a79d012be307e74a5fc77ec248963b93d
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-24 17:12:11 +0200

    feat: ✨ Add service form component to registration process

--- src/app/features/cv-manager/components/register/register.component.ts
+++ src/app/features/cv-manager/components/register/register.component.ts
@@ -3,18 +3,18 @@ import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
 import { ResumeFormComponent } from './resume-form/resume-form.component';
 import { SummaryFormComponent } from './summary-form/summary-form.component';
+import { ServiceFormComponent } from './service-form/service-form.component';
 
 @Component({
   selector: 'app-register',
-  imports: [AppIdDirective, ProfileFormComponent, ResumeFormComponent, SummaryFormComponent],
+  imports: [AppIdDirective, ProfileFormComponent, ResumeFormComponent, SummaryFormComponent, ServiceFormComponent],
   template: `
     <app-profile-form appId="profile-component"></app-profile-form>
     <app-resume-form appId="resume-component"></app-resume-form>
     <app-summary-form appId="summary-component"></app-summary-form>
-<!--    <app-service-form appId="service-component"></app-service-form>-->
+    <app-service-form appId="service-component"></app-service-form>
   `,
 })
 export class RegisterComponent {
 }
 
-//   ServiceFormComponent
