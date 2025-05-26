8351fe9d217aa0f22fdaf7abd51d4c964206b6c8
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-05-26 12:14:51 +0200

    feat: ✨ Add testimonials form component with HTML and SCSS files

---
+++ src/app/features/cv-manager/components/register/testimonials-form/testimonials-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials-form',
+  imports: [],
+  templateUrl: './testimonials-form.component.html',
+  styleUrl: './testimonials-form.component.scss',
+})
+export class TestimonialsFormComponent {}
