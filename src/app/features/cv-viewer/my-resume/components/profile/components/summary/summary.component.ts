ffb11dd52763bdb07437492b3b6c983041f1f6a4
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-06-11 17:17:23 +0200

    feat: ✨ Refactor summary component to use computed properties for education and work experience, enhancing readability and performance

--- src/app/features/cv-viewer/my-resume/components/profile/components/summary/summary.component.ts
+++ src/app/features/cv-viewer/my-resume/components/profile/components/summary/summary.component.ts
@@ -1,14 +1,21 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { Summary } from './interfaces/summary';
 import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AppIdDirective],
+  imports: [AppIdDirective, DatePipe],
 })
 export class SummaryComponent {
   summarySet = input<Summary>();
+  public summary = computed(() => this.summarySet());
+  public education = computed(() => this.summary()?.education?.academicBackground);
+  public workExperience = computed(() => this.summary()?.workExperience?.position);
+
+  public hasEducation = computed(() => (this.education()?.length ?? 0) > 0);
+  public hasWorkExperience = computed(() => (this.workExperience()?.length ?? 0) > 0);
 }
