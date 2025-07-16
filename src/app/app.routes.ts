94d02ec8f55f941859adb61b02e03c5ac0ed7625
Henk Sandoval <henk.sandoval@outlook.com>
2025-07-16 20:17:41 +0200

    fix: 🔧 Update routing structure to include language parameter and adjust redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,22 @@ import { MyResumeComponent } from './features/cv-viewer/my-resume/my-resume.comp
 
 export const routes: Routes = [
   {
-    path: 'cv-manager',
-    loadChildren: () => import('./features/cv-manager/cv-manager.routes').then((m) => m.cvManagerRoutes),
-  },
-  {
-    path: ':slug',
-    component: MyResumeComponent,
+    path: ':lang',
+    children: [
+      {
+        path: 'cv-manager',
+        loadChildren: () => import('./features/cv-manager/cv-manager.routes').then((m) => m.cvManagerRoutes),
+      },
+      {
+        path: ':slug',
+        component: MyResumeComponent,
+      },
+    ]
   },
   {
     path: '',
-    redirectTo: 'cv-manager/register',
-    pathMatch: 'full',
+    redirectTo: '/es/cv-manager/register',
+    pathMatch: 'full'
   },
   {
     path: '**',
